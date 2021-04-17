cf56ce37180a50ca8c7496ab9b861feb60d81bf9
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-04-17 19:54:02 +0700

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -4,22 +4,22 @@
 #include <wait.h>
 
 int main() {
-  pid_t child_id;
-  int status;
+    pid_t child_id;
+    int status;
 
-  child_id = fork();
+    child_id = fork();
 
-  if (child_id < 0) {
-   exit(EXIT_FAILURE);
-  }
+    if (child_id < 0) {
+        exit(EXIT_FAILURE);
+    }
 
-  if(child_id == 0) {
-   char *argv[] = {"mkdir", "-p", "/home/ahmad/modul2/petshop", NULL};
-   execv("/bin/mkdir", argv);
-  }
-   else {
-   while((wait(&status)) > 0);
-   char *argv[] = {"unzip","/home/ahmad/Downloads/pets.zip", "-d", "/home/ahmad/modul2/petshop", NULL };
-   execv("/usr/bin/unzip",argv);
-  }
+    if(child_id == 0) {
+        char *argv[] = {"mkdir", "-p", "/home/ahmad/modul2/petshop", NULL};
+        execv("/bin/mkdir", argv);
+    }
+    else {
+        while((wait(&status)) > 0);
+        char *argv[] = {"unzip","/home/ahmad/Downloads/pets.zip", "-d", "/home/ahmad/modul2/petshop", NULL };
+        execv("/usr/bin/unzip",argv);
+    }
 }
