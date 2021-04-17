54f2763ad7770e866a9cf19b51463d3725f88f32
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-04-17 20:28:10 +0700

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,25 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <wait.h>
+#include <dirent.h>
+#include <string.h>
 
 int main() {
-    pid_t child_id;
-    int status;
+  	pid_t child, child;
+  	int status, status2;
 
-    child_id = fork();
+  	child = fork();
 
-    if (child_id < 0) {
-        exit(EXIT_FAILURE);
-    }
+  	if (child < 0) {
+   		exit(EXIT_FAILURE);
+  	}
 
-    if(child_id == 0) {
-        char *argv[] = {"mkdir", "-p", "/home/ahmad/modul2/petshop", NULL};
-        execv("/bin/mkdir", argv);
-    }
-    else {
-        while((wait(&status)) > 0);
-        char *argv[] = {"unzip","/home/ahmad/Downloads/pets.zip", "-d", "/home/ahmad/modul2/petshop", NULL };
-        execv("/usr/bin/unzip",argv);
-    }
+  	if(child == 0) {
+   		char *argv[] = {"mkdir", "-p", "/home/ahmad/modul2/petshop", NULL};
+   		execv("/bin/mkdir", argv);
+  	}
+   	else {
+   		while((wait(&status)) > 0);
+		child = fork();
+		if(child == 0) {
+   			char *argv[] = {"unzip","/home/ahmad/Downloads/pets.zip", "-d", "/home/ahmad/modul2/petshop", NULL };
+   			execv("/usr/bin/unzip",argv);
+		}
+		else {
+			while((wait(&status2)) > 0);
+			
+		}
+  	}
 }
